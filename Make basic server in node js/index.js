038a6300f201687759790e19f539270c231908ef
Aniket <aniketwankar004@gmail.com>
2022-09-07 19:33:33 +0530

    first server aniket

--- Make basic server in node js/index.js
+++ Make basic server in node js/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http
   .createServer((req, resp) => {
-    resp.write("<h1>Code Step by step</h1>");
+    resp.write("<h1>Aniket wankar first Node server</h1>");
     resp.end();
   })
   .listen(4500);
